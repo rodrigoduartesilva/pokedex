48e34e0c68c8173aa6f636527e6b04426aa40dd8
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-10-30 11:21:50 -0300

    Aqruivo poke-api.js criado

---
+++ assets/js/poke-api.js
@@ -0,0 +1,18 @@
+
+const pokeApi = {}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+}
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) => {
+    console.log(results);
+})
