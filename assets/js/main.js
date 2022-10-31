9005a2b8bb676ee16f4e5378e465b6c698ccea85
Rodrigo Pereira Duarte da Silva <rodrigoduartesilva@yahoo.com.br>
2022-10-30 22:45:16 -0300

    Primeira parte finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,48 @@
-
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
-}
-
-function convertPokemonToLi(pokemon) {
-    return `
-            <li class="pokemon">
-                <span class="number">#${pokemon.order}</span>
-                <span class="name">${pokemon.name}</span>
-
-                <div class="detail">
-                    <ol class="types">
-                        ${convertPokemonTypesToLi(pokemon.types).join('')}
-                    </ol>
-
-                    <img src="${pokemon.sprites.other.dream_world.front_default}"
-                        alt="${pokemon.name}">
-                </div>
-            </li>
-    `
-}
-
 const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    const newHtml = pokemons.map(convertPokemonToLi).join('');
-    pokemonList.innerHTML = newHtml;
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+        
+                        <div class="detail">
+                            <ol class="types">
+                                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                            </ol>
+        
+                            <img src="${pokemon.photo}"
+                                alt="${pokemon.name}">
+                        </div>
+                    </li>
+        `).join('');
+
+        pokemonList.innerHTML += newHtml;
+    });
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+
+    const qtdRecordNexPage = offset + limit
+
+    if (qtdRecordNexPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+
+    loadPokemonItens(offset, limit);
 });
